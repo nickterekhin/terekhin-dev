d20efea5a9f0c7c17c42085d3446b5d5301ef5e5
Nick Terekhin <nick.terekhin@gmail.com>
2017-12-12 01:05:25 +0200

    hide admin bar

--- functions.php
+++ functions.php
@@ -7,3 +7,7 @@ wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  
 wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+
+//hide admin bar
+add_filter('show_admin_bar', '__return_false');
